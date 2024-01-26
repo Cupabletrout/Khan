5761fa70865d6db5b3ec5c7f78d84f8abb590264
Cupabletrout <jh.enterface@gmail.com>
2024-01-25 21:50:36 -0600

    Inventory Styles update

--- src/screens/inventory/inventoryScrn.js
+++ src/screens/inventory/inventoryScrn.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {View, Text } from 'react-native';
-
+import { styles, andriod } from './styles';
 const InventoryScrn =() => {
     return(
-        <View>
+        <View style={[styles.mainContnr, andriod]}>
             <Text>Inventory Screen</Text>
         </View>
     );
