95913861d678de0507b54079a2680799aa098b2e
Cupabletrout <jh.enterface@gmail.com>
2024-01-25 21:19:11 -0600

    +Inventory Folder

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import InventoryScrn from './src/screens/inventory';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <InventoryScrn />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
